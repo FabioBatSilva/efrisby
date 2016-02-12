12aedfe179503220f9f8239ad1227f861ccdf5eb
FabioBatSilva <fabio.bat.silva@gmail.com>
2016-02-12 15:31:13 -0500

    update tests

--- test/efrisby_tests.erl
+++ test/efrisby_tests.erl
@@ -1,9 +1,24 @@
 -module(efrisby_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-get_request_test() ->
-    inets:start(),
+efrisby_test_() ->
+    {setup,
+        fun setup/0,
+        fun teardown/1,
+        [
+            fun get_request/0,
+            fun put_request/0,
+            fun post_request/0
+        ]
+    }.
 
+setup() ->
+    inets:start().
+
+teardown(_) ->
+    inets:stop().
+
+get_request() ->
     ?assertEqual(ok, efrisby:get("http://httpbin.org/get?foo=bar", [
         {status, 200},
         {content_type, "application/json"},
@@ -37,9 +52,7 @@ get_request_test() ->
         ]}
     ])).
 
-post_request_test() ->
-    inets:start(),
-
+post_request() ->
     Body = {
         [
             {<<"foo">>,<<"bar">>}
@@ -62,9 +75,7 @@ post_request_test() ->
 
     ?assertEqual(ok, efrisby:post("http://httpbin.org/post", Body, Headers, Expectations)).
 
-put_request_test() ->
-    inets:start(),
-
+put_request() ->
     Body = {
         [
             {<<"foo">>, <<"bar">>}
